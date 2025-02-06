dbfd27646735e8b50f3769a26c795e06866c623b
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-02-05 20:34:55 -0400

    Add GameController module
    This module will manage the game loop and update the UI. For now all it does is render each player board.

---
+++ src/modules/game-controller.js
@@ -0,0 +1,40 @@
+import { Player } from "./player.js";
+
+export class GameController {
+  constructor(humanSelector, computerSelector, gameTextSelector) {
+    this.humanElem = document.querySelector(humanSelector);
+    this.computerElem = document.querySelector(computerSelector);
+
+    const gameTextElem = document.querySelector(gameTextSelector);
+    this.turnIndicator = gameTextElem.querySelector(".current-turn");
+    this.statusText = gameTextElem.querySelector(".status");
+
+    this.turnIndicator.textContent = "";
+    this.statusText.textContent = "";
+
+    this.domManager = new DOMHelper();
+    this.human = new Player();
+    this.computer = new Player();
+
+    this.domManager.renderBoard(this.human, this.humanElem);
+    this.domManager.renderBoard(this.computer, this.computerElem);
+  }
+}
+
+class DOMHelper {
+  renderBoard(player, playerElem) {
+    const gameboard = playerElem.querySelector(`.gameboard`);
+    const boardMap = player.getBoardMap();
+
+    for (let [key, value] of boardMap.entries()) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      if (value.ship) cell.classList.add("occupied");
+      if (value.isShot) cell.classList.add("attacked");
+
+      cell.dataset.coords = key;
+
+      gameboard.append(cell);
+    }
+  }
+}
